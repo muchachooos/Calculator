f544fdbcd3bda3d18fd8566c9a12ffd0daa6d48c
muchachooos <odnorogg@bk.ru>
2022-07-14 17:25:34 +0300

    Update main.go

--- main.go
+++ main.go
@@ -6,36 +6,32 @@ import (
 )
 
 func main() {
-	a := "+"
-	b := "-"
-	c := "/"
-	d := "*"
 	var j string
 	var x float64
 	var y float64
 
-	fmt.Print("Что делаем?")
+	fmt.Println("Что делаем?")
 	fmt.Fscan(os.Stdin, &j)
 
-	fmt.Print("Введите первое число.")
+	fmt.Println("Введите первое число.")
 	fmt.Fscan(os.Stdin, &x)
 
-	fmt.Print("Введите второе число.")
+	fmt.Println("Введите второе число.")
 	fmt.Fscan(os.Stdin, &y)
 
-	if j == a {
-		fmt.Println(x + y)
-	}
+	switch j {
+	case "-":
+		fmt.Println("Ответ:", x-y)
 
-	if j == b {
-		fmt.Println(x - y)
-	}
+	case "+":
+		fmt.Println("Ответ:", x+y)
 
-	if j == c {
-		fmt.Println(x / y)
-	}
+	case "*":
+		fmt.Println("Ответ:", x*y)
 
-	if j == d {
-		fmt.Println(x * y)
+	case "/":
+		fmt.Println("Ответ:", x/y)
+	default:
+		fmt.Println("Но но но.")
 	}
 }
