7db93a7f0026398b222140785b782db64fe94d91
muchachooos <odnorogg@bk.ru>
2022-07-14 16:20:40 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,8 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Ebala")
-	fmt.Println("Ebala2")
+	a := "+"
+	b := "-"
+	c := "/"
+	d := "*"
+	var j string
+	var x float64
+	var y float64
+
+	fmt.Print("Что делаем?")
+	fmt.Fscan(os.Stdin, &j)
+
+	fmt.Print("Введите первое число.")
+	fmt.Fscan(os.Stdin, &x)
+
+	fmt.Print("Введите второе число.")
+	fmt.Fscan(os.Stdin, &y)
+
+	if j == a {
+		fmt.Println(x + y)
+	}
+
+	if j == b {
+		fmt.Println(x - y)
+	}
+
+	if j == c {
+		fmt.Println(x / y)
+	}
+
+	if j == d {
+		fmt.Println(x * y)
+	}
 }
