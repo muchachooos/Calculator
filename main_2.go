45c3492c183a27bfd35152ec3db1ab1b498ad782
muchachooos <odnorogg@bk.ru>
2022-07-27 17:18:00 +0300

    Update main_2.go

--- main_2.go
+++ main_2.go
@@ -2,40 +2,37 @@ package main
 
 import "fmt"
 
-type element struct {
+type Element struct {
 	value       int
-	nextElement *element
+	nextElement *Element
 }
 
 func main() {
-	element4 := element{
+	element4 := Element{
 		4,
 		nil,
 	}
 
-	element3 := element{
+	element3 := Element{
 		3,
 		&element4,
 	}
 
-	element2 := element{
+	element2 := Element{
 		2,
 		&element3,
 	}
 
-	element1 := element{
+	element1 := Element{
 		1,
 		&element2,
 	}
+	print(&element1)
+}
 
-	element1.nextElement = &element2
-	element2.nextElement = &element3
-	element3.nextElement = &element4
-
-	var c *element = &element1
-
-	for c != nil {
-		fmt.Println(c.value)
-		c = c.nextElement
-	}
+func print(root *Element) {
+	fmt.Println(root.value)
+	fmt.Println(root.nextElement.value)
+	fmt.Println(root.nextElement.nextElement.value)
+	fmt.Println(root.nextElement.nextElement.nextElement.value)
 }
